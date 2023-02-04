0dc22d76ac34cf621d3c62e0728594bc03b715f2
Oliver Hahn <oliver.a.hahn@gmail.com>
2023-02-04 18:56:35 +1100

    Add export databases method

--- generate_databases.py
+++ generate_databases.py
@@ -1,29 +1,57 @@
 import pandas as pd
 from data_cleaning import clean_data
 import numpy as np
+import os
 
 
-def generate_species_lookup(dataframe):
+def generate_species_lookup(detections):
     # Create a new database with a list of unqiue species names
     species_lookup = pd.DataFrame({
-        'species_name': dataframe['species_name'],
-        'species_category': dataframe['species_category']
+        'species_name': detections['species_name'],
+        'species_category': detections['species_category']
     })
     species_lookup = species_lookup.drop_duplicates(subset=['species_name'])
     # Create a primary key
     species_lookup['speciesID'] = np.arange(len(species_lookup))
     # Reorder the columns to have the primary key in the first position and export
     species_lookup = species_lookup[['speciesID', 'species_name', 'species_category']]
-    species_lookup.to_csv('species_lookup.csv', index=False)
 
     # Add in the species ID key to the original detections database
-    dataframe = pd.merge(dataframe, species_lookup[['speciesID', 'species_name']], on='species_name',
-                         validate='many_to_one')
+    detections = pd.merge(detections, species_lookup[['speciesID', 'species_name']], on='species_name',
+                          validate='many_to_one')
     # Remove species category and species name from detections database
-    return dataframe.drop(columns=['species_category', 'species_name'])
+    detections = detections.drop(columns=['species_category', 'species_name'])
+    return detections, species_lookup
+
+
+def generate_species_category_lookup(species_lookup):
+    # Create a new database with a list of unqiue species categories
+    species_category_lookup = pd.DataFrame({'species_category': species_lookup['species_category']})
+    species_category_lookup = species_category_lookup.drop_duplicates()
+    # Create a primary key column
+    species_category_lookup['species_categoryID'] = np.arange(len(species_category_lookup))
+    # Reorder the columns to have the primary key in the first position
+    species_category_lookup = species_category_lookup[['species_categoryID', 'species_category']]
+
+    # Add in the species category ID key to the original species lookup database
+    species_lookup = pd.merge(species_lookup, species_category_lookup, on='species_category', validate='many_to_one')
+    # Remove species category and species name from detections database
+    species_lookup = species_lookup.drop(columns=['species_category'])
+    return species_lookup, species_category_lookup
+
+
+def export_databases(detections, species_lookup, species_category_lookup):
+    # Create a folder for the databases
+    if not os.path.exists('./databases'): os.mkdir('./databases')
+    # Export each to this folder
+    detections.to_csv(os.path.join('./databases', 'detections.csv'), index=False)
+    species_lookup.to_csv(os.path.join('./databases', 'species_lookup.csv'), index=False)
+    species_category_lookup.to_csv(os.path.join('./databases', 'species_category_lookup.csv'), index=False)
 
 
 if __name__ == '__main__':
     det_match = pd.read_csv('det_match.csv')
     detections = clean_data(det_match)
-    detections = generate_species_lookup(detections)
+    detections, species_lookup = generate_species_lookup(detections)
+    species_lookup, species_category_lookup = generate_species_category_lookup(species_lookup)
+    export_databases(detections, species_lookup, species_category_lookup)
