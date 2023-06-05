1e13dc8a2cd43eb5feeb2bd154fda4801736a926
oahahn <oliver.a.hahn@gmail.com>
2023-06-05 11:04:16 +1000

    Add download files conditional

--- generate_databases.py
+++ generate_databases.py
@@ -1,4 +1,4 @@
-from download_gdrive_csvs import download_databases
+import gdrive
 from data_cleaning import clean_data
 from generate_survey_lookup import generate_survey_lookup
 from generate_videos_database import generate_videos_database
@@ -8,8 +8,10 @@ import os
 import argparse
 
 
-def generate_databases(args):
-    # download_databases()
+def generate_databases(args, download_databases):
+    if download_databases:
+        gdrive.download_files()
+        
     # If the location for the new databases doesn't exist, create it
     if not os.path.isdir(args.new_csv_dir):
         os.makedirs(args.new_csv_dir)
@@ -28,4 +30,4 @@ if __name__ == '__main__':
     parser.add_argument('-od', '--old_csv_dir', default='old-csvs')
     parser.add_argument('-nd', '--new_csv_dir', default='new-csvs')
     args = parser.parse_args()
-    generate_databases(args)
+    generate_databases(args, download_databases=True)
