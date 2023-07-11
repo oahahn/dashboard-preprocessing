dfdf7477a23ded19bf6c92e9895e52c9ecaf28e1
Oliver Hahn <oliver.a.hahn@gmail.com>
2023-07-12 09:49:59 +1000

    Update pilot database

--- generate_pilot_lookup.py
+++ generate_pilot_lookup.py
@@ -3,13 +3,78 @@ import os
 import numpy as np
 
 
-pilot_list = ['Matt Clements', 'Chad Beranek', 'Lachlan Hall', 'Sabrina Velasco', 'Pearce Thomas', 'George Madani',
-              'Brian Adam', 'Adam Baus', 'Sam Turbill', 'Fin Murphey', 'Finlay Murphey', 'Nathan Browne', 'Shelby Ryan',
-              'Ryan Witt', 'Andrew Baker', 'Stephen Mahony', 'Shaymus Gooley', 'Adrian Sujaraj', 'Sophie Hall',
-              'Paul Manzoni', 'Robbie Birkett', 'Liam Stephen', 'Tim Jessop', 'Madison Casley', 'Renae Hockey',
-              'Allen McIlwee', 'Elliot Webb', 'Ben Hope', 'Maquel Brandimarti', 'Jared Wood', 'Adam Roff',
-              'Samantha Sanders', 'Oliver Brynes', 'Jarrod Daly', 'Tim Johnson', 'Shawn Ryan', 'Jarrad Prangell',
-              'Kevin Fallon', 'Darryn ?', 'James Tawdrous', 'Sam Provost', 'Hayden ?']
+pilot_list = [
+    # First Intake
+    'Brian Adam',
+    'Fin Murphey',
+    'George Madani',
+    'Lachlan Hall',
+    'Matt Clements',
+    'Nathan Browne',
+    'Pearce Thomas',
+    # NPWS
+    'Adam Baus',
+    'Andrew Baker',
+    # UON
+    'Chad Beranek',
+    'Ryan Witt',
+    'Sabrina Velasco',
+    'Sam Turbill',
+    'Shelby Ryan',
+    'Lachlan Howell',
+    # KS Intake
+    'Liam Stephen',
+    'Stephen Mahony',
+    'Shaymus Gooley',
+    'Adrian Sujaraj',
+    'Sophie Hall',
+    'Paul Manzoni',
+    'Robbie Birkett',
+    # Koala Strat
+    'Tim Jessop',
+    'Ben Hope',
+    'Elliot Webb',
+    'Renae Hockey',
+    'Allen McIlwee',
+    'Madison Caseley',
+    # UON Intake
+    'Samantha Sanders',
+    'Oliver Brynes',
+    'Jarrod Daly',
+    'Tim Johnson',
+    'Shawn Ryan',
+    'Jarrad Prangell',
+    'Kevin Fallon',
+    # KS intake 2
+    'James Mitchell-Williams',
+    'Alison Shilling',
+    'Jeffrey McKee',
+    'Robert Johnston',
+    'Bastian Steinrucken',
+    'Matthew Elsley',
+    'Kieran Richardt',
+    'Jake Smith-Maloney',
+    'Kade Slater',
+    'Freya Dodd',
+    'Matthew Harvey',
+    'Jedd Browne',
+    # KS intake 2 extra
+    'Josh Whitehead',
+    'Nazly Teller',
+    'Charlotte Rigolot',
+    'Wally Hammond',
+    'Rebecca Seeto',
+    # Hub
+    'Adam Roff',
+    'Maquel Brandimarti',
+    'Jared Wood',
+    # Never Active
+    'James Tawdrous',
+    'Hayden Griffith',
+    'Sam Provost',
+    'Daryn McKenny',
+]
+
 
 def generate_pilot_lookup(new_csvs):
     pilot_lookup = pd.DataFrame({
